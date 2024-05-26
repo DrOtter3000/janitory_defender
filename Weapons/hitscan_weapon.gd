5da2a4da1e5440846d9c16234f7e402865296c0b
DrOtter3000 <info@darknet-online.com>
2024-05-26 23:27:02 +0200

    add muzzle flash

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -5,10 +5,12 @@ extends Node3D
 @export var recoil := 0.05
 @export var weapon_mesh: Node3D
 @export var weapon_damage := 15
+@export var muzzle_flash: GPUParticles3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +25,7 @@ func _process(delta: float) -> void:
 func shoot() -> void:
 	cooldown_timer.start(1.0/fire_rate)
 	
+	muzzle_flash.restart()
 	weapon_mesh.position.z += recoil
 	
 	var collider = ray_cast_3d.get_collider()
