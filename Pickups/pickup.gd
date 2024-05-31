1b24d0efce8c99065cc27ac23d644e41aaac61ed
DrOtter3000 <info@darknet-online.com>
2024-05-31 22:06:30 +0200

    make pickups disappear over time

--- Pickups/pickup.gd
+++ Pickups/pickup.gd
@@ -4,6 +4,14 @@ extends Area3D
 @export var ammo_type: AmmoHandler.ammo_type
 @export var amount: int = 20
 
+var size_reducer := 0.01
+var about_to_die := false
+
+
+func _process(delta: float) -> void:
+	if about_to_die:
+		print("ouchie")
+		die()
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
@@ -11,3 +19,11 @@ func _on_body_entered(body: Node3D) -> void:
 		queue_free()
 
 
+func _on_life_timer_timeout() -> void:
+	about_to_die = true
+
+
+func die() -> void:
+	scale -= Vector3(size_reducer, size_reducer, size_reducer)
+	if scale < Vector3.ZERO:
+		queue_free()
--- Pickups/scrap.gd
+++ Pickups/scrap.gd
@@ -1,7 +1,30 @@
 extends Area3D
 
 
+@export var ammo_type: AmmoHandler.ammo_type
+@export var amount: int = 20
+
+var size_reducer := 0.01
+var about_to_die := false
+
+
+func _process(delta: float) -> void:
+	if about_to_die:
+		print("ouchie")
+		die()
+
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		body.scrap += 5
+		queue_free()
+
+
+func _on_life_timer_timeout() -> void:
+	about_to_die = true
+
+
+func die() -> void:
+	scale -= Vector3(size_reducer, size_reducer, size_reducer)
+	if scale < Vector3.ZERO:
 		queue_free()
