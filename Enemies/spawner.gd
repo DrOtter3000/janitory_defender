2cd503dbb80caa99ddc110ce492aa59ea11c6b63
DrOtter3000 <info@darknet-online.com>
2024-06-02 19:14:44 +0200

    fix vanishing enemies bug

--- Enemies/spawner.gd
+++ Enemies/spawner.gd
@@ -12,8 +12,8 @@ func _ready() -> void:
 
 func spawn_enemy() -> void:
 	var new_enemy = enemy.instantiate()
-	add_child(new_enemy)
-	new_enemy.position = Vector3(randi_range(-10, 10),0,0)
+	get_node("..").add_child(new_enemy)
+	new_enemy.global_position = position + Vector3(randi_range(-10, 10),0,0)
 
 
 func _on_enemy_spawn_timer_timeout() -> void:
